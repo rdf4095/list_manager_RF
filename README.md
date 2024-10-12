d0ee0cb8d200f7d79ca88dea6b7eac052712c070
rdf4095 <112559769+rdf4095@users.noreply.github.com>
2024-10-11 20:06:53 -0400

    Add function sort_category

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# todo_RF
+# list_manager_RF
+
+This application is for interactive creation and formatting of small lists.
+It is more formal than necessary for the purppose, but demonstrates communication between
+toplevel windows, and moving textual information from one kind of widget to another.
+
+There are no program dependencies except, on linux you may need to install tkinter.
+
+Two toplevel windows are created. In the first, list items are created in
+a structured way, using Combobox and Entry widgets. Rows can be added or removed from
+the interface (UI) as necessary.
+Spacers can be defined to separate rows in the formatted output.
+
+Upon a button click, text is collected from the UI:
+    - item category, selected from a the Combobox list
+    - item text, entered by the user
+Collected text is written to the output window, each UI row providing a line of output.
+Lines are numbered, and within each line, category values are separated from text.
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 program: main.py
 
-purpose: Implement a list manager
+purpose: main module for project list_manager_RF: implement a list manager
 
 comments: There are two toplevel windows, one to create the list and 
           one to display it, formatted.
@@ -14,9 +14,14 @@ history:
 09-30-2024  Add second toplevel window, top2.
 10-04-2024  Refine top2. Rename sort_cat to move_text. Add line separator in
             the output. Remove old comments and print()s.
+10-10-2024  Add option buttons for top2, the text output window.
+            Remove debug print statements and unused variables (already 
+            commented out).
+10-11-2024  Add function sort_category to resort output text by item category.
 """
 """
-TODO: - put spacing rules for Tk widgets into a cnf dict.
+TODO: - put common spacing rules for pack() into a cnf dict.
+      - get contents of Text widget text_main.
 """
 import tkinter as tk
 from tkinter import ttk
@@ -25,32 +30,22 @@ from importlib.machinery import SourceFileLoader
 msel = SourceFileLoader("ui_multi_select", "../ui_RF/ui_multi_select.py").load_module()
 sttk = SourceFileLoader("styles_ttk", "../styles/styles_ttk.py").load_module()
 
-def opt_fxn():
-    move_text(item_rows)
+def move_text(source: list) -> None:
+    """Get text from widgets and move it to another toplevel window."""
 
+    category_list = [item.winfo_children()[0].get() for item in item_rows]
+    text_list = [item.winfo_children()[1].get() for item in item_rows]
 
-def move_text(source):
-    # get text
-    # category_list = [item.winfo_children()[0].get() for item in item_rows]
-    # text_list = [item.winfo_children()[1].get() for item in item_rows]
+    text_main = top2.winfo_children()[0].winfo_children()[0]
+
     category_list = [item.winfo_children()[0].get() for item in source]
     text_list = [item.winfo_children()[1].get() for item in source]
 
-    # put text in output window
     text_main.delete('1.0', 'end')
     linenum = 1
     for n, i in enumerate(category_list):
-        # st = str(n+1) + "-" + i +  ": " + text_list[n]
-
-        # end_pt = text_main.index('end')
-        # insert_pt = end_pt + ' + 1c'
         separator = '--'
 
-        # if i != '' and text_list[n] != '':
-        #     text_main.insert('end', st)
-        #     text_main.insert('end', '\n')
-        # else:
-        #     text_main.insert('end', '\n')
         if i == '':
             if text_list[n] == '':
                 text_main.insert('end', '\n')
@@ -65,6 +60,61 @@ def move_text(source):
                 text_main.insert('end', '\n')
                 linenum += 1
 
+    # print(f'text_main is a{type(text_main)}')
+    
+
+def sort_category():
+    """Sort text by item category."""
+    print('in option1')
+    
+    alltext = text_main.get('1.0', 'end-1c')
+    # print(f'alltext:\n{alltext}')
+
+    line_list = alltext.split('\n')
+    # print(f'line 1:\n{line_list[0]}')
+
+    # test    
+    # this is crude. regex is a better tool, but trades off complexity.
+    # category_list = [line.split('-')[1].split(':')[0] for line in line_list[:-1]]
+    # print(f'first line category:\n{category_list[0]}')
+
+    line_list_unnum = [line.split('-')[1] for line in line_list[:-1]]
+    print(f'lines, unnumbered:\n{line_list_unnum}')
+    print()
+
+    sort_cat_list = sorted(line_list_unnum)
+    print(f'lines, sorted:\n{sort_cat_list}')
+
+    # replace output with sorted list
+    text_main.delete('1.0', 'end')
+    linenum = 1
+    # works; try it without the n
+    for n, i in enumerate(sort_cat_list):
+        # separator = '--'
+
+        # if i == '':
+        #     if text_list[n] == '':
+        #         text_main.insert('end', '\n')
+        #     else:
+        #         if text_list[n] == '-':
+        #             text_main.insert('end', separator)
+        #             text_main.insert('end', '\n')
+        # else:
+            # if text_list[n] != '':
+            #     st = str(linenum) + "-" + i +  ": " + text_list[n]
+            #     text_main.insert('end', st)
+            #     text_main.insert('end', '\n')
+            #     linenum += 1
+            # if text_list[n] != '':
+                st = str(linenum) + "-" + i
+                text_main.insert('end', st)
+                text_main.insert('end', '\n')
+                linenum += 1
+
+
+def option2():
+    print('in option2')
+
 
 # Module scope objects
 # ====================
@@ -74,26 +124,18 @@ root.title('list manager')
 sttk.CreateStyles()
 
 item_rows = []
-
 filt_cboxes = []
 filt_entries = []
 filt_buttons_add = []
 filt_buttons_subt = []
 
-# used in ui_multi_select functions, but
-# not needed for this app
-# windows = {'one': None, 'two': None}
-
 data_columns = ['home', 'work', 'hobby']
-my_fxn = None
+# my_fxn = None
 
 main_lab = ttk.Label(root, foreground='blue', border=2, text='Create List')
 main_lab.pack(anchor='w', padx=5)
 
 main_list_fr = ttk.Frame(root, border=2)
-
-# text1 = ttk.Label(root, background='#ff0', text='build list')
-# text1.pack(anchor='n')
 
 category_label = ttk.Label(root, background='#ff0', text='category')
 category_label.pack(anchor='w', padx=15)
@@ -121,39 +163,42 @@ btnq.configure(style='MyButton1.TButton')
 btnq.pack(anchor='s', pady=10)
 
 root.update()
-rgeom = root.geometry()
-# print(f'root geometry: {rgeom}')
-geom_str = rgeom.split('x')
+
+geom_str = root.geometry().split('x')
 win_wd = geom_str[0]
 win_ht = geom_str[1].split('+')[0]
-# print(f'root width/height: {win_wd}, {win_ht}')
 
 win_hoff = geom_str[1].split('+')[1]
 win_voff = geom_str[1].split('+')[2]
-# print(f'root h,v offsets: {win_hoff}, {win_voff}')
 
 # Toplevel window 2
-# -----------------
+# =================
 top2 = tk.Toplevel()
 top2.title('list viewer')
-# top2.geometry("+300+250")
+
 top2_h_offset = str(int(win_wd) + int(win_hoff))
 top2_v_offset = win_ht
-top2_offs_str = "+" + top2_h_offset + "+" + top2_v_offset
-# print(top2_offs_str)
-top2.geometry(top2_offs_str)
+offset_string = "+" + top2_h_offset + "+" + top2_v_offset
 
-# options_fr = ttk.Frame(frm1_2, relief='groove')
-# options_fr.pack(padx=5, pady=5, ipadx=5, ipady=5)
+top2.geometry(offset_string)
 
-# win_label = ttk.Label(options_fr, text="label frame",
-#                      style="EntryLabel.TLabel")
-# win_label.pack(padx=5, pady=5)
-
-main_fr = ttk.LabelFrame(top2, text="Formatted List")
+lab2 = ttk.Label(top2, text="Formatted List", style="LabelFrameText.TLabel")
+main_fr = ttk.LabelFrame(top2, labelwidget=lab2)
 main_fr.pack(padx=5, pady=5)
+
+options_fr = ttk.Frame(top2, relief='groove')
+options_fr.pack(padx=5, pady=5, ipadx=5, ipady=5)
+
+opt1_but = ttk.Button(options_fr, text="sort category", command=sort_category)
+opt1_but.pack(side='left', padx=5, pady=5)
+
+opt2_but = ttk.Button(options_fr, text="option 2", command=option2)
+opt2_but.pack(side='left', padx=5, pady=5)
 
 text_main = tk.Text(main_fr, width=40, height=10, background='#ffa')
 text_main.pack(padx=5, pady=5)
 
-root.mainloop()
+# print(f'item_rows[0] child 0 is {type(item_rows[0].winfo_children()[0])}')
+
+if __name__ == "__main__":
+    root.mainloop()
